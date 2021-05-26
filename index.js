35323759035e8c7eb6b48d7bed87171218697c1a
ZawadiSumba66 <rosesumba98@gmail.com>
2021-05-26 21:01:57 +0300

    Fix linters errors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const menu = document.querySelector('.menu-bars')
-const nav = document.querySelector('#desktop-nav')
+const menu = document.querySelector('.menu-bars');
+const nav = document.querySelector('#desktop-nav');
 
-menu.addEventListener('click',()=>{
-    nav.classList.toggle('nav-active');
-})
+menu.addEventListener('click', () => {
+  nav.classList.toggle('nav-active');
+});
